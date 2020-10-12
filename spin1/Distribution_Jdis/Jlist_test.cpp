b598f76eb06e431d90d936b680e85e7f32cfa876
ShaoFuLiu <zx22920416@gmail.com>
2020-10-12 17:40:56 +0800

    20201012 add Dimer distribution

--- spin1/Distribution_Jdis/Jlist_test.cpp
+++ spin1/Distribution_Jdis/Jlist_test.cpp
@@ -33,24 +33,27 @@ double Distribution_Random_Variable(int model, double var, double Jdis)
 int main()
 {
     double Jdis = 0.1; //= delta
-    double dimer = 0.5;
+    double dim = 0; //=dimer
     int delta = Jdis * 10;
+    int dimer = dim * 10;
     int L = 10000;
     uniform_real_distribution<double> Dist_J(nextafter(0.0, 1.0), 1.0);
     mt19937 genFixed(1);
-    vector<double> J_list; 
+    vector<double> J_list, J_list2; 
     
     for (int i = 0; i < L; i++)
     {
-        double jvar = Dist_J(genFixed); // all values are same prob (0 to 1).
-        jvar = (1 + dimer*pow(-1,i+1)) * Distribution_Random_Variable(10, jvar, Jdis);
-        //jvar = jvar * (1 + dimer*pow(-1,i+1)) * Distribution_Random_Variable(20, jvar, Jdis);
+        double jvar = Dist_J(genFixed); // all values are same prob (0 to 1) xi
+        double jvar2 = Dist_J(genFixed) - 0.5; // all values are same prob (-0.5 to 0.5) ita(i)
+        jvar = (1 + dim*pow(-1,i+1)) * Distribution_Random_Variable(10, jvar, Jdis); // xi^delta
+        jvar2 = (1 + dim*pow(-1,i+1)) * Distribution_Random_Variable(20, jvar, Jdis); // exp(ita(i)*delta)
         J_list.push_back(jvar);
+        J_list2.push_back(jvar2);
     }
 
     // creat file to save J_list
     string file;
-    file = "./J"+ to_string(delta) +"_list.csv";
+    file = "./J"+ to_string(delta) + "D" + to_string(dimer) + "_list.csv";
     ofstream fout(file);              // == fout.open(file);
     if (!fout)
     {
@@ -58,10 +61,10 @@ int main()
         err << "Error: Fail to save (maybe need mkdir " << file << ")";
         throw runtime_error(err.str());
     }
-    fout << "J" << endl;
+    fout << "J,J2" << endl;
     for (int i = 0; i < J_list.size(); i++)
     {
-        fout << J_list[i] << endl;
+        fout << J_list[i] << "," << J_list2[i] << endl;
     }
 
     return 0;
--- spin1/Distribution_Jdis/Plot_Jlist_distribution.py
+++ spin1/Distribution_Jdis/Plot_Jlist_distribution.py
@@ -10,32 +10,43 @@ from scipy.optimize import curve_fit
 
 L = 10000
 Jdis = 'J1'
-myfile = '/home/liusf/test/spin1/Distribution_Jdis/'+ Jdis +'_list.csv'
-df = pd.read_csv(myfile)
-
-up = 0
-down = 0.05
-n = 0.05
-df2 = pd.DataFrame(columns = ['mid', 'Prob'])
-
-for j in range(19):
-    #print((left+right)/2)
-    mid = (up+down)/2 
-    selected = df[df['J'].between(up,down)]['J']
-    up += n
-    down += n
-    Prob = len(selected)/L
-    mean = {'mid':mid ,'Prob':Prob}  
-    df2.loc[j] = mean
-
-plt.plot(df2['mid'], df2['Prob'], 'o-', markersize = 8)
-
+Dimer = ['D0', 'D1', 'D2', 'D3', 'D4', 'D5']
+for i in range(len(Dimer)):
+    myfile = '/home/liusf/test/spin1/Distribution_Jdis/'+ Jdis + Dimer[i] +'_list.csv'
+    df = pd.read_csv(myfile)
+    up = 0
+    down = 0.05
+    n = 0.05
+    df1 = pd.DataFrame(columns = ['mid', 'Prob'])
+    df2 = pd.DataFrame(columns = ['mid', 'Prob'])
+    """
+    plt.plot(np.array(df['J2']), 'o', markersize = 8)
+    print(np.array(df['J2']))
+    """
+    for j in range(40):
+        #selected1 = df[df['J'].between(up,down)]['J']
+        selected2 = df[df['J2'].between(up,down)]['J2']
+        mid = (up+down)/2 
+        up += n
+        down += n
+        #Prob1 = len(selected1)/L
+        Prob2 = len(selected2)/L
+        #mean1 = {'mid':mid ,'Prob':Prob1}
+        mean2 = {'mid':mid ,'Prob':Prob2}
+        #df1.loc[j] = mean1
+        df2.loc[j] = mean2
+#   plt.plot(df1['mid'], df1['Prob'], 'o-', markersize = 8)
+#   plt.plot(df2['mid'], df2['Prob'], 'o-', markersize = 8, label = 'Dimer = %s' %(Dimer[i]))
+df['J2'].hist()
+"""
 plt.xlabel('$J$', fontsize=14)
 plt.ylabel('$P(J)$', fontsize=14)
-plt.title(Jdis +'_power-law_distribution', fontsize=12)
-plt.xlim(0,1)
+plt.title(Jdis + 'D1toD5_power-law_distribution', fontsize=12)
+#plt.xlim(0,1)
 #plt.ylim(0,0.1)
-#plt.legend(loc = 'best', fontsize=12)
+plt.legend(loc = 'best', fontsize=12)
 plt.grid()
-plt.savefig('./'+ Jdis +'_power-law_distribution.pdf', format='pdf', dpi=4000)
+#plt.savefig('/home/liusf/test/spin1/Distribution_Jdis/'+ Jdis + Dimer +'_power-law_distribution.pdf', format='pdf', dpi=4000)
+plt.savefig('/home/liusf/test/spin1/Distribution_Jdis/D1toD5_distribution.pdf', format='pdf', dpi=4000)
 plt.show()
+"""
