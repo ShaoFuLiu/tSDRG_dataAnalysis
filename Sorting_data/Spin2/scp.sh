fbb089524e52269780c6ec5293171d3fb38d6a21
ShaoFuLiu <zx22920416@gmail.com>
2021-09-02 13:28:25 +0800

    20210902

--- Sorting_data/Spin2/scp.sh
+++ Sorting_data/Spin2/scp.sh
@@ -1,1 +1,2 @@
 scp -r liusf@scopion.phys.nthu.edu.tw:/home/liusf/tSDRG/Sorting_data/Spin2/metadata/* ./metadata/
+/home/liusf/tSDRG/Sorting_data/Spin2_new
---
+++ Sorting_data/scp-1.sh
@@ -0,0 +1,2 @@
+scp -r liusf@scopion.phys.nthu.edu.tw:/home/liusf/tSDRG/Sorting_data/Spin1/metadata/* ./Spin1_new/metadata/
+
